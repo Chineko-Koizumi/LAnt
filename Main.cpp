8756d3418d32a0eb3f82524cbe768a8e4695131e
Chineko-Koizumi <przprz24@interia.pl>
2023-02-08 20:12:43 +0100

    witout terminal GUI

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,5 @@
 #include "Mesh.h"       // field for ant and ant itself
 #include "FileParser.h" // main function argument parrser
-#include "Menus.h"      // ftxui menus
 
 static uint32_t  WINDOW_WIDTH  = 0;
 static uint32_t  WINDOW_HEIGHT = 0;
@@ -46,104 +45,84 @@ int main(int argc, char* argv[])
 
     sf::Color* colors = da::FileParser::CreateColorArray(CSV); // parsed colors for mesh from arguments
 
-    sf::Event event; // for windows event pool
-	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "ANt", true);
+    bool IsOnlyGenerating = false;
+    if (argc > 5)IsOnlyGenerating = true;
 
-    da::Menu menu = da::Menu();
+    sf::Event event; // for windows event pool
+	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "GraphApp", true );
 
     da::Mesh mesh(WINDOW_WIDTH, WINDOW_HEIGHT, &window, atoi(argv[4]));
     da::Ant ant(&mesh, colors, WINDOW_WIDTH, WINDOW_HEIGHT);
 
     ant.SetOffset( WINDOW_WIDTH / 2 - 20, WINDOW_HEIGHT / 2);
 
-#pragma region MainLoop
-    uint8_t MenuOption=0;
-
-    while(true)
-    switch (MenuOption)
+#pragma region Window
+    uint64_t Progress = 0;
+    if (IsOnlyGenerating) 
     {
-        case da::MAIN:
+        while (window.isOpen())
         {
-            MenuOption = menu.Main();
-        }break;
-        case da::ANT_GUI:
-        {
-            window.setActive(true);
-            while (window.isOpen())
+            if (window.pollEvent(event))
             {
-                if (window.pollEvent(event))
+                switch (event.type)
                 {
-                    switch (event.type)
-                    {
                     case sf::Event::KeyPressed:
                     {
                         switch (event.key.code)
                         {
-                        case sf::Keyboard::Escape:
-                        {
-                            da::FileParser::DeleteColorArray();
-                            mesh.DumpToFileAndExit();
-                        }break;
-                        case sf::Keyboard::Right:
-                        {
-                            da::KeyboardMethods::SpeedUpRender();
-                        }break;
-                        case sf::Keyboard::Left:
-                        {
-                            da::KeyboardMethods::SpeedDownRender();
-                        }break;
+                            case sf::Keyboard::Escape:
+                            {
+                                mesh.DumpToFileAndExit();
+                            }break;  
                         }
                     }break;
-                    }
-                }
-
-                mesh.DrawMesh();
-                window.display();
-
-                for (size_t i = 0; i < da::KeyboardMethods::m_RenderStepCount; i++)
-                {
-                    ant.NextMove();
                 }
             }
-
-        }break;
-
-        case da::ANT_NOGUI:
-        {
-            uint64_t Progress = 0;
-            while (window.isOpen())
-            {
-                if (window.pollEvent(event))
-                {
-                    switch (event.type)
-                    {
-                    case sf::Event::KeyPressed:
-                    {
-                        switch (event.key.code)
-                        {
-                        case sf::Keyboard::Escape:
-                        {
-                            mesh.DumpToFileAndExit();
-                        }break;
-                        }
-                    }break;
-                    }
-                }
-                for (size_t i = 0; i < 10000000; i++) ant.NextMove();
-                std::cout << std::endl << (++Progress) * 10000000;
-            }
-
-        }break;
-        case da::EXIT:
-        {
-            da::FileParser::DeleteColorArray();
-            return 0;
-        }break;
-
-        default:
-            break;
+            for (size_t i = 0; i < 10000000; i++) ant.NextMove();
+            std::cout<< std::endl << (++Progress) * 10000000;
+        }
     }
+    else 
+    {
+        window.setActive(true);
+        while (window.isOpen())
+        {
+            if (window.pollEvent(event))
+            {
+                switch (event.type)
+                {
+                    case sf::Event::KeyPressed:
+                    {
+                        switch (event.key.code)
+                        {
+                            case sf::Keyboard::Escape:
+                            {
+                                mesh.DumpToFileAndExit();
+                            }break;
+                            case sf::Keyboard::Right:
+                            {
+                                da::KeyboardMethods::SpeedUpRender();
+                            }break;
+                            case sf::Keyboard::Left:
+                            {
+                                da::KeyboardMethods::SpeedDownRender();
+                            }break;
+                        }
+                    }break;
+                }
+            }
 
+            mesh.DrawMesh();
+            window.display();
+
+            for (size_t i = 0; i < da::KeyboardMethods::m_RenderStepCount; i++)
+            {
+                ant.NextMove();
+            }
+        }
+    }
 #pragma endregion
+
+    da::FileParser::DeleteColorArray();
 	return 0;
 }
